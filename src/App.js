5698ef481ef9ef14d4936e5f9535b6cc8c343ff5
keener2u <keener2u@penguin>
2020-07-07 22:24:42 -0700

    timer countdown and topic array change feature added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,48 @@ import React from 'react';
 
 
 class App extends React.Component{
+    state = {seconds: 5, topic: "loading"};
+    agenda = [
+        {
+            title: 'topic 2',
+            time: 20
+        },
+        {
+            title: 'topic 1',
+            time: 10
+        } 
+    ];
+
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            const { seconds } = this.state
+
+            if (seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            }
+            if (seconds === 0) {
+                    this.nextTopic();
+            } 
+        }, 1000);
+    }
+
+    nextTopic = () => {
+        
+        const {time,title} = this.agenda.pop()
+        this.setState({seconds: time, topic: title})
+
+    }
+
+
     render(){
-        return <div>Test</div>
+        return(
+            <div>
+                <h1>Topic {this.state.topic} </h1>
+            <div>{this.state.seconds}</div>
+            </div>
+        );
     }
 }
 
